a38fb460b28d4212f3086344e8dc62216ef1897d
jawadahmedshahani <70814895+jawadahmedshahani@users.noreply.github.com>
2020-09-09 07:30:41 -0700

    Add files via upload

---
+++ whil loop counttttt.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=2;
+	int b=3;
+	int ct=0;
+	while(a<b)
+	{
+		ct++;
+		cout<<ct<<"="<<"javad"<<endl;
+		a++;
+		if(a==b)
+		{
+			break;
+		}
+		
+	}
+	return 0;
+}
